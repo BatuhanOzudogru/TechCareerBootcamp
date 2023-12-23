a38537a8e5f688bee559a6cfebb74732940f78d7
BatuhanOzudogru <batuhan.ozudogru@hotmail.com>
2023-12-23 21:18:24 +0300

    Task 2

---
+++ Task2/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace Task2.Models
+{
+    public class Category
+    {
+        public Category(int id,string name)
+        {
+            this.Name = name;
+            this.Id = id;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
